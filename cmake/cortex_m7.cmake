8cf22f88daefccdb3fd8a1f71e1f729abc4e2632
wt <wt@xx.com>
2024-12-17 16:53:07 +0800

    Revert "update: cmake"
    This reverts commit dee99fe64b29efbe70bc8a10d54437b02a6ebe2c.
    
    fix: discard

--- cmake/cortex_m7.cmake
+++ cmake/cortex_m7.cmake
@@ -5,9 +5,9 @@ set(CMAKE_SYSTEM_PROCESSOR cortex-m7)
 set(MCPU_FLAGS "-mthumb -mcpu=cortex-m7")
 set(VFP_FLAGS  "-mfloat-abi=hard -mfpu=fpv5-d16")
 
+include (${CMAKE_CURRENT_LIST_DIR}/common_opt.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/toolchain.cmake)
+
 # Threadx
 set(THREADX_ARCH "cortex_m7")
 set(THREADX_TOOLCHAIN "gnu")
-
-# Helper
-include(${CMAKE_CURRENT_LIST_DIR}/environment.cmake)
