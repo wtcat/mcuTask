a6570357593fb692ead6ac97b930e697dfa85478
wt <wt@xx.com>
2024-12-20 12:18:02 +0800

    update

--- cmake/cortex_m4.cmake
+++ cmake/cortex_m4.cmake
@@ -7,8 +7,6 @@ set(VFP_FLAGS  "-mfloat-abi=hard -mfpu=fpv4-sp-d16")
 
 include (${CMAKE_CURRENT_LIST_DIR}/common_opt.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/toolchain.cmake)
-set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} -std=gnu17")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++23")
 
 # Threadx
 set(THREADX_ARCH "cortex_m4")
--- cmake/cortex_m7.cmake
+++ cmake/cortex_m7.cmake
@@ -7,8 +7,6 @@ set(VFP_FLAGS  "-mfloat-abi=hard -mfpu=fpv5-d16")
 
 include(${CMAKE_CURRENT_LIST_DIR}/common_opt.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/toolchain.cmake)
-set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} -std=gnu17")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++23")
 
 # Threadx
 set(THREADX_ARCH "cortex_m7")
