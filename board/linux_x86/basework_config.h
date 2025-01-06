83e4b62dbe396987356785a3b25dbfb198f6a098
wt <wt@xx.com>
2025-01-06 09:50:04 +0800

    update

---
+++ board/linux_x86/basework_config.h
@@ -0,0 +1,9 @@
+#ifndef basework_config_h_
+#define basework_config_h_
+
+#define CONFIG_OS_MAX_TIMERS 2
+#define CONFIG_OS_MAX_FILES  3
+
+#define CONFIG_C11_MEM_MODEL 1
+
+#endif /* basework_config_h_ */
