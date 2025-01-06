0bce7a687821efcf5a3b9ee3565d037a5e3a0a5f
wt <wt@xx.com>
2025-01-06 13:03:14 +0800

    update

--- board/linux_x86/main.c
+++ board/linux_x86/main.c
@@ -1,8 +1,10 @@
 /*
  * Copyright 2024 wtcat
  */
-
+#include <stdio.h>
 #include "tx_api.h"
+
+#include "subsys/fs/fs.h"
 
 #define MAIN_THREAD_PRIO  11
 #define MAIN_THREAD_STACK 4096
@@ -14,10 +16,15 @@ UINT _kernel_byte_pool_size = KERNEL_HEAP_SIZE;
 static TX_THREAD main_pid;
 static ULONG main_stack[MAIN_THREAD_STACK / sizeof(ULONG)];
 
+static void file_test(void);
 
+static void stdio_puts(const char *s, size_t len) {
+    fwrite(s, len, 1, stdout);
+}
 
 int main(int argc, char *argv[]) {
 
+    __console_puts = stdio_puts;
     /* Enter the ThreadX kernel.  */
     tx_kernel_enter();
     return 0;
@@ -35,6 +42,8 @@ static void main_thread(void *arg) {
     do_sysinit();
     tx_thread_preemption_change(pid, new, &old);
 
+    file_test();
+
     for ( ; ; ) {
         tx_thread_sleep(TX_MSEC(10000));
     }
@@ -43,4 +52,39 @@ static void main_thread(void *arg) {
 void tx_application_define(void *unused) {
     tx_thread_spawn(&main_pid, "main_thread", main_thread, &main_pid, main_stack, 
         sizeof(main_stack), MAIN_THREAD_PRIO, MAIN_THREAD_PRIO, TX_NO_TIME_SLICE, TX_AUTO_START);
+}
+
+
+static void file_test(void) {
+    int err;
+
+    err = fs_mkfs(FS_EXFATFS, "ramblk", NULL, 0);
+    if (err)
+        return;
+
+    err = fs_mount("/", "ramblk", FS_EXFATFS, 0, NULL);
+    if (err)
+        return;
+
+    struct fs_file fd = {0};
+    err = fs_open(&fd, "/hello.txt", FS_O_CREATE | FS_O_WRITE);
+    if (err)
+        goto _unmount;
+
+    fs_write(&fd, "hello world", 11);
+    fs_close(&fd);
+
+    struct fs_file rfd = {0};
+    char buffer[12] = {0};
+
+    err = fs_open(&rfd, "/hello.txt", FS_O_READ);
+    if (err)
+        goto _unmount;
+
+    fs_read(&rfd, buffer, 11);
+    fs_close(&rfd);
+
+_unmount:
+    fs_unmount("/");
+    return;
 }
---
+++ board/linux_x86/ram_blkdev.c
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2024 wtcat
+ */
+
+#define pr_fmt(fmt) "[ramblk]: "fmt
+#include <string.h>
+
+#include "tx_api.h"
+#include "drivers/blkdev.h"
+
+#define CONFIG_RAMBLK_SIZE 512
+#define CONFIG_RAMBLK_MEMORY_SIZE 0x100000
+
+static char ram_blk_memory[CONFIG_RAMBLK_MEMORY_SIZE];
+
+static int 
+ram_blkdev_request(struct device *dev, struct blkdev_req *req) {
+    switch (req->op) {
+    case BLKDEV_REQ_READ:
+        memcpy(req->buffer, &ram_blk_memory[req->blkno * CONFIG_RAMBLK_SIZE], 
+            req->blkcnt * CONFIG_RAMBLK_SIZE);
+        return 0;
+    case BLKDEV_REQ_WRITE:
+        memcpy(&ram_blk_memory[req->blkno * CONFIG_RAMBLK_SIZE], req->buffer, 
+            req->blkcnt * CONFIG_RAMBLK_SIZE);
+        return 0;
+    default:
+        break;
+    }
+    return -EINVAL;
+}
+
+static int
+ram_blkdev_control(struct device *dev, unsigned int cmd, void *arg) {
+    switch (cmd) {
+    case BLKDEV_IOC_GET_BLKSIZE:
+        return CONFIG_RAMBLK_SIZE;
+
+    case BLKDEV_IOC_GET_BLKCOUNT:
+        return CONFIG_RAMBLK_MEMORY_SIZE / CONFIG_RAMBLK_SIZE;
+
+    default:
+        return -EINVAL;
+    }
+}
+
+static struct block_device ram_blkdev = {
+    .name = "ramblk",
+    .request = ram_blkdev_request,
+    .control = ram_blkdev_control
+};
+
+static int ram_blkdev_int(void) {
+    int err;
+
+    err = device_register((struct device *)&ram_blkdev);
+    if (!err) {
+        pr_info("%s register success\n", ram_blkdev.name);
+    }
+    
+    return err;
+}
+
+SYSINIT(ram_blkdev_int, SI_DRIVER_LEVEL, 10);
