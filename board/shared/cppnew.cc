351a5b04c790c4e178f69a0ecb1da9beb1e56c91
wt <wt1454246140@163.com>
2025-05-16 10:22:17 +0800

    fix: cpp memory allocate

--- board/shared/cppnew.cc
+++ board/shared/cppnew.cc
@@ -7,6 +7,10 @@
 #include <stdlib.h>
 #include <new>
 
+#include <base/assert.h>
+#include <service/malloc.h>
+#include <service/heap.h>
+
 #if __cplusplus < 201103L
 #define NOEXCEPT
 #else /* >= C++11 */
@@ -19,68 +23,85 @@
 #define NODISCARD [[nodiscard]]
 #endif /* __cplusplus */
 
+#if CONFIG_CPP_POOL_SIZE > 0
+static BYTE_HEAP_DEFINE(cpp_pool, CONFIG_CPP_POOL_SIZE, );
+
+#define CC_MALLOC(_size)               __kasan_heap_allocate(&cpp_pool, size, TX_NO_WAIT)
+#define CC_ALIGN_ALLOC(_align, _size)  __kasan_heap_allocate(&cpp_pool, size, TX_NO_WAIT)
+#define CC_FREE(_ptr)                  __kasan_heap_free(_ptr)
+
+#else
+#define CC_MALLOC(_size)                kmalloc(_size, 0)
+#define CC_ALIGN_ALLOC(_align, _size)   kmalloc(_size, 0)
+#define CC_FREE(_ptr)                   kfree(_ptr)
+#endif /* CONFIG_CPP_HEAP_SIZE > 0 */
+
 NODISCARD void* operator new(size_t size)
 {
-	return malloc(size);
+	return CC_MALLOC(size);
 }
 
 NODISCARD void* operator new[](size_t size)
 {
-	return malloc(size);
+	return CC_MALLOC(size);
 }
 
 NODISCARD void* operator new(std::size_t size, const std::nothrow_t& tag) NOEXCEPT
 {
-	return malloc(size);
+	return CC_MALLOC(size);
 }
 
 NODISCARD void* operator new[](std::size_t size, const std::nothrow_t& tag) NOEXCEPT
 {
-	return malloc(size);
+	return CC_MALLOC(size);
 }
 
 #if __cplusplus >= 201703L
 NODISCARD void* operator new(size_t size, std::align_val_t al)
 {
-	return aligned_alloc(static_cast<size_t>(al), size);
+	rte_assert((size_t)al <= sizeof(void *));
+	return CC_ALIGN_ALLOC(static_cast<size_t>(al), size);
 }
 
 NODISCARD void* operator new[](std::size_t size, std::align_val_t al)
 {
-	return aligned_alloc(static_cast<size_t>(al), size);
+	rte_assert((size_t)al <= sizeof(void *));
+	return CC_ALIGN_ALLOC(static_cast<size_t>(al), size);
 }
 
 NODISCARD void* operator new(std::size_t size, std::align_val_t al,
 			     const std::nothrow_t&) NOEXCEPT
 {
-	return aligned_alloc(static_cast<size_t>(al), size);
+	rte_assert((size_t)al <= sizeof(void *));
+	return CC_ALIGN_ALLOC(static_cast<size_t>(al), size);
 }
 
 NODISCARD void* operator new[](std::size_t size, std::align_val_t al,
 			       const std::nothrow_t&) NOEXCEPT
 {
-	return aligned_alloc(static_cast<size_t>(al), size);
+	rte_assert((size_t)al <= sizeof(void *));
+	return CC_ALIGN_ALLOC(static_cast<size_t>(al), size);
 }
 #endif /* __cplusplus >= 201703L */
 
 void operator delete(void* ptr) NOEXCEPT
 {
-	free(ptr);
+	CC_FREE(ptr);
 }
 
 void operator delete[](void* ptr) NOEXCEPT
 {
-	free(ptr);
+	CC_FREE(ptr);
 }
 
 #if (__cplusplus > 201103L)
 void operator delete(void* ptr, size_t) NOEXCEPT
 {
-	free(ptr);
+	CC_FREE(ptr);
 }
 
 void operator delete[](void* ptr, size_t) NOEXCEPT
 {
-	free(ptr);
+	CC_FREE(ptr);
 }
 #endif // __cplusplus > 201103L
